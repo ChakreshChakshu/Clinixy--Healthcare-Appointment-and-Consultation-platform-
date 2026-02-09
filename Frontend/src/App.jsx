426946cf094d82e0b053263ce402a0ff39be8559
Rishu148 <dhakedroopkishor@gmail.com>
2026-02-09 11:23:33 +0530

    login register ui ready

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,26 +1,7 @@
-import { useEffect, useState } from "react";
-import api from "./services/api";
+import AppRoutes from "./routes/AppRoutes";
 
-function App() {
-  const [message, setMessage] = useState("");
-
-  useEffect(() => {
-    api.get("/health")
-      .then((res) => {
-        setMessage(res.data.status);
-      })
-      .catch((err) => {
-        console.error(err);
-        setMessage("Clinixy backend is not running");
-      });
-  }, []);
-
-  return (
-    <div style={{ padding: "40px" }}>
-      <h1>Clinixy</h1>
-      <p style={{ color: "green" }}>{message}</p>
-    </div>
-  );
-}
+const App = () => {
+  return <AppRoutes />;
+};
 
 export default App;
